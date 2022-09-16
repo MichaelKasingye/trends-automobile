67e15ce5bbfdac2308dbddee14df716f5a9259f6
michaelkasingye <michaelkasingye@gmail.com>
2022-09-16 15:26:21 +0300

    Feat: Change nav and hero

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -3,37 +3,24 @@ import React from 'react'
 export default function Index() {
   return (
     <>
-  {/* ======= Hero Section ======= */}
-  <section id="hero" className="hero d-flex align-items-center">
-    <div className="container">
-      <div className="row">
-        <div className="col-xl-4">
-          <h2 data-aos="fade-up">Focus On What Matters</h2>
-          <blockquote data-aos="fade-up" data-aos-delay={100}>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Perspiciatis cum recusandae eum laboriosam voluptatem repudiandae
-              odio, vel exercitationem officiis provident minima.{" "}
-            </p>
-          </blockquote>
-          <div className="d-flex" data-aos="fade-up" data-aos-delay={200}>
-            <a href="#about" className="btn-get-started">
-              Get Started
-            </a>
-            <a
-              href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
-              className="glightbox btn-watch-video d-flex align-items-center"
-            >
-              <i className="bi bi-play-circle" />
-              <span>Watch Video</span>
-            </a>
-          </div>
-        </div>
+    {/* ======= Hero Section ======= */}
+    <section
+      id="hero"
+      className="d-flex flex-column justify-content-center align-items-center"
+    >
+      <div className="container text-center text-md-left" data-aos="fade-up">
+        <h1>
+          Welcome to <span>Trends Auto</span>
+        </h1>
+        <h2>We are team of talented designers making websites with Bootstrap</h2>
+        <a href="#about" className="btn-get-started scrollto">
+          Get Started
+        </a>
       </div>
-    </div>
-  </section>
-  {/* End Hero Section */}
-</>
+    </section>
+    {/* End Hero */}
+  </>
+  
 
   )
 }
--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -3,91 +3,113 @@ import React from 'react'
 export default function Index() {
   return (
     <>
-  {/* ======= Header ======= */}
-  <header id="header" className="header d-flex align-items-center fixed-top">
-    <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
-      <a href="index.html" className="logo d-flex align-items-center">
-        {/* Uncomment the line below if you also wish to use an image logo */}
-        {/* <img src="assets/img/logo.png" alt=""> */}
-        <h1 className="d-flex align-items-center">Nova</h1>
-      </a>
-      <i className="mobile-nav-toggle mobile-nav-show bi bi-list" />
-      <i className="mobile-nav-toggle mobile-nav-hide d-none bi bi-x" />
-      <nav id="navbar" className="navbar">
-        <ul>
-          <li>
-            <a href="index.html" className="active nav-link">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="about.html" className='nav-link'>About</a>
-          </li>
-          <li>
-            <a href="services.html" className='nav-link'>Services</a>
-          </li>
-          <li>
-            <a href="portfolio.html"className='nav-link'>Portfolio</a>
-          </li>
-          <li>
-            <a href="team.html"className='nav-link'>Team</a>
-          </li>
-          <li>
-            <a href="blog.html"className='nav-link'>Blog</a>
-          </li>
-          <li className="dropdown">
-            <a href="#"className='nav-link'>
-              <span>Dropdown</span>{" "}
-              <i className="bi bi-chevron-down dropdown-indicator" />
-            </a>
-            <ul>
-              <li>
-                <a href="#"className='nav-link'>Dropdown 1</a>
-              </li>
-              <li className="dropdown">
-                <a href="#"className='nav-link'>
-                  <span>Deep Dropdown</span>{" "}
-                  <i className="bi bi-chevron-down dropdown-indicator" />
-                </a>
-                <ul>
-                  <li>
-                    <a href="#"className='nav-link'>Deep Dropdown 1</a>
-                  </li>
-                  <li>
-                    <a href="#"className='nav-link'>Deep Dropdown 2</a>
-                  </li>
-                  <li>
-                    <a href="#"className='nav-link'>Deep Dropdown 3</a>
-                  </li>
-                  <li>
-                    <a href="#"className='nav-link'>Deep Dropdown 4</a>
-                  </li>
-                  <li>
-                    <a href="#"className='nav-link'>Deep Dropdown 5</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#"className='nav-link'>Dropdown 2</a>
-              </li>
-              <li>
-                <a href="#"className='nav-link'>Dropdown 3</a>
-              </li>
-              <li>
-                <a href="#">Dropdown 4</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="contact.html"className='nav-link'>Contact</a>
-          </li>
-        </ul>
-      </nav>
-      {/* .navbar */}
-    </div>
-  </header>
-  {/* End Header */}
-</>
+    {/* ======= Header ======= */}
+    <header id="header" className="fixed-top d-flex align-items-center">
+      <div className="container d-flex align-items-center">
+        <div className="logo me-auto">
+          <h1>
+            <a href="index.html">Trends Auto</a>
+          </h1>
+          {/* Uncomment below if you prefer to use an image logo */}
+          {/* <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>*/}
+        </div>
+        <nav id="navbar" className="navbar order-last order-lg-0">
+          <ul>
+            <li>
+              <a className="nav-link scrollto active" href="#hero">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="nav-link scrollto" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="nav-link scrollto" href="#services">
+                Services
+              </a>
+            </li>
+            <li>
+              <a className="nav-link scrollto " href="#portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a className="nav-link scrollto" href="#testimonials">
+                Testimonials
+              </a>
+            </li>
+            <li className="dropdown">
+              <a href="#">
+                <span>Drop Down</span> <i className="bi bi-chevron-down" />
+              </a>
+              <ul>
+                <li>
+                  <a href="#">Drop Down 1</a>
+                </li>
+                <li className="dropdown">
+                  <a href="#">
+                    <span>Deep Drop Down</span>{" "}
+                    <i className="bi bi-chevron-right" />
+                  </a>
+                  <ul>
+                    <li>
+                      <a href="#">Deep Drop Down 1</a>
+                    </li>
+                    <li>
+                      <a href="#">Deep Drop Down 2</a>
+                    </li>
+                    <li>
+                      <a href="#">Deep Drop Down 3</a>
+                    </li>
+                    <li>
+                      <a href="#">Deep Drop Down 4</a>
+                    </li>
+                    <li>
+                      <a href="#">Deep Drop Down 5</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Drop Down 2</a>
+                </li>
+                <li>
+                  <a href="#">Drop Down 3</a>
+                </li>
+                <li>
+                  <a href="#">Drop Down 4</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a className="nav-link scrollto" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <i className="bi bi-list mobile-nav-toggle" />
+        </nav>
+        {/* .navbar */}
+        <div className="header-social-links d-flex align-items-center">
+          <a href="#" className="twitter">
+            <i className="bi bi-twitter" />
+          </a>
+          <a href="#" className="facebook">
+            <i className="bi bi-facebook" />
+          </a>
+          <a href="#" className="instagram">
+            <i className="bi bi-instagram" />
+          </a>
+          <a href="#" className="linkedin">
+            <i className="bi bi-linkedin" />
+          </a>
+        </div>
+      </div>
+    </header>
+    {/* End Header */}
+  </>
+  
 
   )
 }
